3fe908b14aec4014b48a446f4a0739ae98382bee
Shankar Natarajan <shankarvn@gmail.com>
2016-04-03 01:09:00 -0700

    More string array problems.

--- arraysandstrings/main.js
+++ arraysandstrings/main.js
@@ -39,3 +39,79 @@ console.log(areCharsUniqueUsingBitVector(testStr1));
 var testStr2 = "abc";
 console.log(areCharsUnique(testStr2));
 console.log(areCharsUniqueUsingBitVector(testStr2));
+
+//*********************************************************
+
+function reverseStr(str){
+	var revStr = "";
+	for (var i=str.length-1;i>=0;i--){
+		revStr += str[i];
+	}
+	return revStr;
+}
+
+var testStr3 = "afdasdkahsdkahd";
+console.log(reverseStr(testStr3));
+
+//*********************************************************
+
+function case_insensitive_comp(strA, strB) {
+    return strA.toLowerCase().localeCompare(strB.toLowerCase());
+}
+
+function isStringPermutationOfAnother(s1, s2){
+	var s1Arr = s1.split("");
+	var s2Arr = s2.split("");
+
+	var s1sorted = Array.prototype.sort.call(s1Arr, case_insensitive_comp).join("");
+	var s2sorted = Array.prototype.sort.call(s2Arr, case_insensitive_comp).join("");
+
+	return (s1sorted == s2sorted);
+
+}
+
+console.log(isStringPermutationOfAnother("abcf","fbca"))
+console.log(isStringPermutationOfAnother("abcdf","fbca"))
+
+//*********************************************************
+
+
+
+function stringReplace(str){
+	var strArr = str.split("");
+	var strArrRep = strArr.map(function(each) { if (each == " ") { return "%20" } else {return each} });
+	return strArrRep.join("");
+}
+
+console.log(stringReplace("abc   "));
+
+//*********************************************************
+
+function compressString(str){
+	var prevChar = str[0];
+	var count = 1;
+	var compressedStr = "";
+	for (var i=1;i<str.length;i++){
+		var nextChar = str[i];
+		if (nextChar == prevChar){
+			count++;
+			if (i==str.length-1){
+				compressedStr+=prevChar+count;
+			}
+		}
+		else{
+			compressedStr+=prevChar+count;
+			prevChar = nextChar;
+			count = 1;
+		}
+	}
+	if (compressedStr.length > str.length){
+		return str;
+	}
+	else{
+		return compressedStr;
+	}
+}
+
+console.log(compressString("aaabbbcdeff"));
+console.log(compressString("aabccccccccaaa"));
